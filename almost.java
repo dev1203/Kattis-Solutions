dab594c893c59ab2dc9abf0f369081b8c3992ca4
dev1203 <devsingh120397@gmail.com>
2018-05-24 20:51:18 -0700

    Adding new Solution

---
+++ almost.java
@@ -0,0 +1,94 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.OutputStream;
+public class almost{
+	public static void main(String []args){
+		Kattio io=new Kattio(System.in);
+		while(io.hasMoreTokens()){
+			int number=io.getInt();
+			int sum=0;
+			for(int i=1;i<=number/2;i++){
+				if(number%i==0){
+					sum+=i;
+				}
+			}
+			if(sum==number){
+				System.out.println(number+" perfect");
+			}
+			else if((sum-2)==number || (sum+2)==number){
+				System.out.println(number+" almost perfect");
+			}
+			else{
+				System.out.println(number+" not perfect")
+;			}
+		}
+
+
+	}
+	
+}
+
+
+
+class Kattio extends PrintWriter {
+    public Kattio(InputStream i) {
+        super(new BufferedOutputStream(System.out));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+    public Kattio(InputStream i, OutputStream o) {
+        super(new BufferedOutputStream(o));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public boolean hasMoreTokens() {
+        return peekToken() != null;
+    }
+
+    public int getInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    public double getDouble() {
+        return Double.parseDouble(nextToken());
+    }
+
+    public long getLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    public String getWord() {
+        return nextToken();
+    }
+
+
+
+    private BufferedReader r;
+    private String line;
+    private StringTokenizer st;
+    private String token;
+
+    private String peekToken() {
+        if (token == null)
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    line = r.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                }
+                token = st.nextToken();
+            } catch (IOException e) { }
+        return token;
+    }
+
+    private String nextToken() {
+        String ans = peekToken();
+        token = null;
+        return ans;
+    }
+}
+
