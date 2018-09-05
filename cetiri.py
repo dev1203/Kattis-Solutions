15de9885cfbf355fbca4175813982ef17d137cbd
dev1203 <devsingh120397@gmail.com>
2018-09-04 21:10:08 -0700

    some minor changes

--- cetiri.py
+++ cetiri.py
@@ -4,4 +4,8 @@ number = []
 for value in data:
     number.append(int(value))
 number.sort()
-print((number[1]-number[0])+number[1])
+one = (number[1]-number[0])
+if(one + number[1]) not in number:
+    print(one + number[1])
+else:
+    print(one + number[2])
