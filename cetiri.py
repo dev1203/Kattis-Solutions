0908e797d40d474d52576a95be1f548b489e7700
dev1203 <devsingh120397@gmail.com>
2018-09-15 13:32:56 -0700

    cetri solution

--- cetiri.py
+++ cetiri.py
@@ -1,11 +1,15 @@
 import sys
+def find_number(number, difference):
+    for value in number:
+        if value + difference not in number:
+            print(value + difference)
+            return
+            
+
 data = input().split()
 number = []
 for value in data:
     number.append(int(value))
 number.sort()
-one = (number[1]-number[0])
-if(one + number[1]) not in number:
-    print(one + number[1])
-else:
-    print(one + number[2])
+difference = number[1] - number[0]
+find_number(number,difference)
